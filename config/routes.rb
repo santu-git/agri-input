b115b13202a29c7502f1ee4ac9fbdc233274d3a0
Avishek Jana <avishekjana.it@gmail.com>
2017-11-17 12:46:10 +0530

    Admin namespace added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 Rails.application.routes.draw do
-  namespace :applicant do
-    resources :warehouses
+  namespace :admin do
+    get '', to: 'welcome#index', as: 'root'
   end
 
-  devise_for :admin_users, path: 'admins'
+  resources :warehouses
+
+  resources :states do
+    resources :districts, except: [:index]
+    resources :districts, only: [:index], defaults: { format: 'json' }
+  end
+
+  root 'warehouses#index'
+
   devise_for :applicant_users, path: 'applicants', controllers: {
     registrations: 'applicant_users/registrations',
     sessions: 'applicant_users/sessions'
   }
-  
+
+  devise_for :admin_users, path: 'admins', controllers: {
+    registrations: 'admin_users/registrations',
+    sessions: 'admin_users/sessions'
+  }
+
   # devise_for :applicant_users, controllers: {
   #   registrations: 'applicant_users/registrations',
   #   sessions: 'applicant_users/sessions'
@@ -18,10 +31,4 @@ Rails.application.routes.draw do
   #   registrations: 'admin_users/registrations',
   #   sessions: 'applicant_users/sessions'
   # }
-
-  resources :states do
-    resources :districts, except: [:index]
-    resources :districts, only: [:index], defaults: { format: 'json' }
-  end
-  root 'welcome#index'
 end

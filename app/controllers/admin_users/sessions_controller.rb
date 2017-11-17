ec6a9a6ba24fb8a0e9381e7add634c469530935d
Avishek Jana <avishekjana.it@gmail.com>
2017-11-17 12:45:09 +0530

    Custom redirect paths added for signin and signout

--- app/controllers/admin_users/sessions_controller.rb
+++ app/controllers/admin_users/sessions_controller.rb
@@ -11,15 +11,18 @@ class AdminUsers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    return '/admin'
+  end
+
+  def after_sign_out_path_for(resource)
+    return '/admins/sign_in'
+  end
 end

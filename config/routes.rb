e935ab692a53246b99cfd7ddc1e069983e909889
Santu Koley <santu.cst08@gmail.com>
2017-11-19 10:30:09 +0530

    Nav link implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get '', to: 'welcome#index', as: 'root'
+    get '', to: 'dashboard#index', as: 'root'
     resources :educations
   end
 

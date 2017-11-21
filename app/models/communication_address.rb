fa9256768196ba1a0baa0220c41af7521779c4d9
shinymajumder <shinymajumder.ingreens@gmail.com>
2017-11-21 14:48:47 +0530

    Validation of Communication address

--- app/models/communication_address.rb
+++ app/models/communication_address.rb
@@ -2,4 +2,10 @@ class CommunicationAddress < ApplicationRecord
   belongs_to :applicant_user
   belongs_to :state
   belongs_to :district
+
+  validates :street_no, presence: true
+  validates :post_office, presence: true
+  validates :police_station, presence: true
+  validates :pin_code, presence: true
+  validates :extended_type, presence: true
 end
